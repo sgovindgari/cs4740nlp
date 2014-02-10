555c25fea35e4aeb41bd9cd7d02619c1eacb89f9
Ben <shulman.benjamin@gmail.com>
2014-02-09 23:08:11 -0500

    Added comments to ngram, fixed mistake in parse

--- proj01/ngram.py
+++ proj01/ngram.py
@@ -1,6 +1,12 @@
 import re, random, time
 from collections import OrderedDict
 
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    return type('Enum', (), enums)
+
+Smooth = enum('NONE', 'GOOD_TURING', 'ADD_ONE')
+
 class unigram():
     #Reads a text file in and converts it into an array
     def __init__(self, sourceFile, n = 1):
@@ -31,7 +37,7 @@ class unigram():
 
 # Generalized n-gram model - O(nN) or something
 class ngram():
-    def __init__(self, sourceFile, n = 1):
+    def __init__(self, sourceFile, n = 1, smooth = None):
         with open(sourceFile) as corpus:
             self.corpus = re.split('\s+', corpus.read())
         # This stores dictionaries for recording counts of the p previous words followed by a word
@@ -48,9 +54,10 @@ class ngram():
             prevs = list()
             for j in range(n):
                 if i-j >= 0:
-                    # Get the previous words
+                    # Add another word to the previous words
                     if(j > 0):
                         prevs.append(self.corpus[i-j])
+                    # Must convert to tuple to hash into dictionary, reverse list to keep words in the correct order
                     lookup = tuple(reversed(prevs))
                     if lookup in self.counts[j]:
                         if word in self.counts[j][lookup]:
@@ -71,17 +78,23 @@ class ngram():
                 for entry in ngram[row]:
                     self.probs[i][row][entry] = ngram[row][entry] / float(total)
 
+    # Sum the values of the dictionary and returns the total
     def _sumDict(self, d):
         total = 0
         for k, v in d.iteritems():
             total += v
         return total
 
+    # Generates a random sentence using this model 
     def randomSentence(self):
+        # Prev stores the previously generated words to consider
         prev = list()
         if self.n != 1:
+            #Should we start with <s>? or nothing? or something else...
             prev = ['<s>']
+        # The words generated thus far are stored in res
         res = list()
+        # Generate words until a sentence segmentor is created
         while True:
             word = ''
             gram = self.probs[len(prev)]
@@ -89,20 +102,20 @@ class ngram():
                 word = self.generateWord(gram[tuple(prev)])
                 prev.append(word)
                 res.append(word)
+                # Maintain only as many previous words as the model allows
                 if len(prev) >= self.n:
                     prev.pop(0)
+            # if the entry doesn't exist, try using the previous t-1 words instead
             else:
                 prev.pop(0)
             if word == '<s>':
                 break
         print ' '.join(res)
 
+    # Given a dictionary of (words, probability) generate a random word drawn from this distribution
     def generateWord(self, row):
         p = random.random()
         for word in row:
             p -= row[word]
             if p < 0:
                 return word
-
-# TODO: How do we generate the first word in a bigram model. Do we use
-# the probability of a period followed by a word, or the probability of <s> followed by a word or the unigram probability?
