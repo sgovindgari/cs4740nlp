ff2794f3f60264fa97f0cdb6cc1012bcf83ccfe2
Ben <shulman.benjamin@gmail.com>
2014-03-09 21:21:58 -0400

    Did some stuff, doesn't really work. Whoops.

--- proj02/naive_bayes.py
+++ proj02/naive_bayes.py
@@ -5,7 +5,7 @@ class NaiveBayes():
         self.senseCounts = dict()
         self.wordCounts = dict()
         self.featureCounts = dict()
-        self.featureList = []
+        self.featureList = dict()
         #Generating necessary counts
         for example in trainingSet:
             word = example[0]
@@ -24,6 +24,8 @@ class NaiveBayes():
                 self.senseCounts[word] = dict()
                 self.senseCounts[word][sense] = 1
             for key,value in features.items():
+                if key not in self.featureList:
+                    self.featureList[key] = key
                 if (word, sense) in self.featureCounts:
                     if (key,value) in self.featureCounts[(word,sense)]:
                         self.featureCounts[(word,sense)][(key,value)] += 1
@@ -39,9 +41,13 @@ class NaiveBayes():
         #Fetch necessary features
         #TODO: This is complete but it struggles with the fact that it doesn't go over all features and also what if a featureCount is zero!?
         #TODO: Feature counts don't hold counts for all features...
-        #TODO: Smooth
+        #TODO: Smooth - using add 1 for now I guess
+        #TODO: HALP
+        actual = []
+        correct = 0
         for example in testSet:
             word = example[0]
+            actual.append(example[1])
             features = example[2]
             probs = []
             if word in self.wordCounts:
@@ -49,25 +55,30 @@ class NaiveBayes():
                 for sense in self.senseCounts[word]:
                     sc = self.senseCounts[word][sense]
                     prob = math.log(sc) - math.log(wc)
-                    for key,value in features.items():
-                        if (key,value) in self.featureCounts[(word,sense)]:
-                            prob += math.log(self.featureCounts[(word,sense)][(key,value)]) - math.log(sc)
+                    for key in self.featureList:
+                        prob -= math.log(sc+1)
+                        if key in features:
+                            value = features[key]
+                            if (key,value) in self.featureCounts[(word,sense)]:
+                                prob += math.log(self.featureCounts[(word,sense)][(key,value)] + 1)
                         else:
-                            #TODO: Can't return 0, if using logs, using logs will cause negative
-                            prob = 0.0
-                            break
+                            #using 1 only works for boolean
+                            if (key,1) in self.featureCounts[(word,sense)]:
+                                prob += math.log(sc - self.featureCounts[(word,sense)][(key,1)] + 1)
+                            else:
+                                prob += math.log(sc+1)
                     probs.append((sense,prob))
                 res = utilities.argmax(probs)
-                if res != 1:
-                    print probs
+                correct = correct if res != example[1] else correct+1
                 predictions.append(res)
             else:
                 predictions.append(-1)
+        print correct/float(len(predictions))
         return predictions
 
 
 # pp = pprint.PrettyPrinter(indent=4)
 # pp.pprint(utilities.constructSet(windowSize=2,loc="temp.pickle")[:5])
 nb = NaiveBayes(utilities.constructSet(windowSize=10))
-print nb.classify(utilities.constructSet(source='test_clean.data'))
+print nb.classify(utilities.constructSet(source='validation_clean.data',windowSize=10))
 
