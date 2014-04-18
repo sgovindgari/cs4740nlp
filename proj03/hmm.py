04eea07b79223049debfecbdb602117ee90407a9
Ben <shulman.benjamin@gmail.com>
2014-04-17 22:10:40 -0400

    initial hmm stuff

---
+++ proj03/hmm.py
@@ -0,0 +1,11 @@
+#
+class HMM():
+    def __init__(self, documents, n):
+        self.documents = documents
+        self.n = n
+
+    #observed is a list of observed variables to tag
+    def viterbi(observations):
+        #dictionary of dictionaries
+        table = []
+        
